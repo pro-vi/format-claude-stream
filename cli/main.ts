0cedf46faaa6001268fc736510596b271b26f24f
Ben Christel <ben.christel@gmail.com>
2026-02-23 16:03:35 -0800

    Implement cat in node

--- cli/main.ts
+++ cli/main.ts
@@ -1,3 +1,17 @@
 #!/usr/bin/env -S node --import @swc-node/register/esm-register
 
-console.log("Hello, world!");
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', (line) => {
+    console.log(line);
+});
+
+rl.once('close', () => {
+
+});
