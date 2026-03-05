1b4edcf0231016ce78137084f0e442adcc195873
Ben Christel <ben.christel@gmail.com>
2026-03-05 12:11:26 -0800

    Prepare index.ts for publication

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {ClaudeStreamFormatter} from "./core/claude-stream-formatter.ts";
+export {ChalkColorizer} from "./adapters/chalk-colorizer.ts";
+export {StandardOutput} from "./adapters/standard-output.ts";
+export type {Colorizer} from "./core/ports/colorizer.ts";
+export type {Output} from "./core/ports/output.ts";
